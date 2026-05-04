e53c1ae885e44f6359ddb4c0a7f44c0fd8596649
Tony Bai <tonybai321@gmail.com>
2026-05-04 14:21:23 -0700

    Test pre-commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+def badFunction():
+    x = 1 + 2
+    print(x)
 
-def badFunction( ):
-	x=1+2
-	print( x )
 
 def main():
     print("Hello from assignment-5!")
 
 
 if __name__ == "__main__":
+    x = 1 + 2
     main()
